fb8edfefaadeacd758a2089681614de7c22149fc
hcruz82 <henri_cruz@hotmail.com>
2021-09-22 11:27:45 -0300

    Primeiro commit do código

---
+++ operadores-relacionais.js
@@ -0,0 +1,27 @@
+/*
+Var
+// Seção de Declarações das variáveis 
+   nota1, nota2: real
+   
+
+Inicio
+// Seção de Comandos, procedimento, funções, operadores, etc... 
+   nota1 :=10
+   nota2 :=10
+   
+         se (nota1 <> nota2) entao
+         escreval ("Nota1 é diferente")
+         senao
+         escreval ("Nota1 é igual")
+         fimse
+         */
+
+         var nota1, nota2
+         nota1 =10
+         nota2 =10
+
+         if (nota1 != nota2)
+         alert ("Nota1 é diferente")
+         else
+         ("Nota1 é igual")
+
